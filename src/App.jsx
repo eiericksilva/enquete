c4a0d87d45afd5f40d8cf240a0ea76b86af26963
eiericksilva <ericksilvaredes@gmail.com>
2023-04-19 16:58:19 -0300

    Falta modificar o contador

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [alternativaA, setAlternativaA] = useState("");
   const [alternativaB, setAlternativaB] = useState("");
   const [alternativaC, setAlternativaC] = useState("");
-  const [inputSelected, setInputSelected] = useState();
+  const [optionSelected, setOptionSelected] = useState("");
 
   useEffect(() => {
     console.log("ENQUETES:", enquetes);
@@ -19,10 +19,12 @@ function App() {
 
     const novaEnquete = {
       id: uuidv4(),
-      pergunta,
-      alternativaA,
-      alternativaB,
-      alternativaC,
+      textQuestion: pergunta,
+      options: [
+        { a: alternativaA, count: 0 },
+        { b: alternativaB, count: 0 },
+        { c: alternativaC, count: 0 },
+      ],
     };
 
     setEnquetes([...enquetes, novaEnquete]);
@@ -40,9 +42,13 @@ function App() {
     setEnquetes(result);
   };
 
-  const registrarResposta = (id, resposta) => {
-    console.log("ID:", id);
-    console.log("RESPOSTA:", resposta);
+  const registrarResposta = (id, response) => {
+    console.log(id);
+    console.log(response);
+  };
+
+  const handleChange = (e) => {
+    setOptionSelected(e.target.value);
   };
 
   return (
@@ -95,50 +101,38 @@ function App() {
       </form>
 
       <hr />
-      {enquetes.map((enquete) => (
-        <div key={enquete.id}>
-          <form>
-            <h3>{enquete.pergunta}</h3>
-            <label>
-              <input
-                type="radio"
-                name={enquete.pergunta}
-                value={enquete.alternativaA}
-                onChange={(e) => setInputSelected(e.target.value)}
-              />
-              {enquete.alternativaA}
-            </label>
-            <label>
-              <input
-                type="radio"
-                name={enquete.pergunta}
-                value={enquete.alternativaB}
-                onChange={(e) => setInputSelected(e.target.value)}
-              />
-              {enquete.alternativaB}
-            </label>
-            <label>
-              <input
-                type="radio"
-                name={enquete.pergunta}
-                value={enquete.alternativaC}
-                onChange={(e) => setInputSelected(e.target.value)}
-              />
-              {enquete.alternativaC}
-            </label>
-            <button
-              onClick={() => registrarResposta(enquete.id, inputSelected)}
-              type="button"
-            >
-              Enviar resposta
-            </button>
-            <button onClick={() => excluirEnquete(enquete.id)}>
-              Excluir Enquete
-            </button>
-            <hr />
-          </form>
-        </div>
-      ))}
+      {enquetes.length < 1 ? (
+        <div>Vazio</div>
+      ) : (
+        enquetes.map((enquete) => (
+          <div key={enquete.id}>
+            <form>
+              <h3>{enquete.textQuestion}</h3>
+              {enquete.options.map((option) => (
+                <label>
+                  <input
+                    type="radio"
+                    name={enquete.textQuestion}
+                    value={option.a || option.b || option.c}
+                    onChange={handleChange}
+                  />
+                  {option.a || option.b || option.c}
+                </label>
+              ))}
+              <button
+                onClick={() => registrarResposta(enquete.id, optionSelected)}
+                type="button"
+              >
+                Enviar resposta
+              </button>
+              <button onClick={() => excluirEnquete(enquete.id)}>
+                Excluir Enquete
+              </button>
+              <hr />
+            </form>
+          </div>
+        ))
+      )}
     </div>
   );
 }
